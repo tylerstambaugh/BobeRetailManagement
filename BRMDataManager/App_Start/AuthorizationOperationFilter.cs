99fc4976180d0e43e1385af81fe62ed9c9dac660
Tyler Stambaugh <11870675+tylerstambaugh@users.noreply.github.com>
2022-11-20 20:21:20 -0500

    added auth token on API reqeusts

---
+++ BRMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,30 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+using System.Web.Http.Filters;
+
+namespace BRMDataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "Access Token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
