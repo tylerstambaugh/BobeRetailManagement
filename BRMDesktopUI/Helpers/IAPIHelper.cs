cab30a5c5ebe0dba856c38ebcf3dd0f871e5a454
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.com>
2022-12-17 12:23:13 -0500

    login form wired up to API use IAPIHelper

---
+++ BRMDesktopUI/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using BRMDesktopUI.Models;
+using System.Threading.Tasks;
+
+namespace BRMDesktopUI.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
