c3afbd07d3bf5505a6fe5fd0b9061d704a81b042
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.com>
2022-12-17 20:09:53 -0500

    getting user data

---
+++ BRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using BRMDataManager.Library.Internal.DataAccess;
+using BRMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "DefaultConnection");
+            
+            return output;
+        }
+    }
+}
---
+++ BRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using BRMDataManager.Library.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BRMDataManager.Controllers
+{
+
+    [Authorize]
+    [RoutePrefix("api/User")]
+    public class UserController : Controller
+    {
+
+        // GET: User/Details/5
+        public ActionResult GetById(string id)
+        {
+            UserData
+
+            return View();
+        }
+
+    }
+}
